ae198111c14c285b438577073210b4a1ff9d4628
Nicolly Bonato <148967268+nicollybonato@users.noreply.github.com>
2024-09-19 22:38:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Vôlei',
+    'Quais são as principais diferenças entre o vôlei de quadra e de praia?',
+    'Número de sets, set de desempate, quadra, competições, número de jogadores e bola.'
+)
+
+criaCartao(
+    'Vôlei',
+    'O que é o rodízio?',
+    'É a troca de posições dos jogadores na quadra, que acontece no sentido horário, após uma equipe marcar um ponto no saque do adversário.'
+)
+
+criaCartao(
+    'Vôlei',
+    'Qual é o nome da posição que é especializada em defesa e utiliza uniforme divergente ao resto da equipe?',
+    'Líbero'
+)
+
+criaCartao(
+    'Beach Tennis',
+    'Quais são os principais fundamentos do Beach Tennis?',
+    'Saque, recepção, voleios, smash e defesa.'
+)
+
+criaCartao(
+    'Futevôlei',
+    'Quais são os fundamentos básicos do futevôlei?',
+    'Saque, recepção, passe, ataque e defesa.'
+)
